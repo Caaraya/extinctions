ed3b0b3bd41bc970858ac6dcc2970c3a21fac79a
Christina Araya <christina-araya@hotmail.com>
2015-02-09 07:33:07 +0000

    Classifications controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'classifications/index'
+
+  get 'classifications/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
