f640328272a3ecdb695bbc6ca5ce32c2ffde97a1
Christina Araya <christina-araya@hotmail.com>
2015-02-11 00:27:12 +0000

    not entirely sure why the extinction date isn't working

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,7 +2,7 @@ class Animal < ActiveRecord::Base
   belongs_to :classification
   has_many :common_names
   def recent?
-    if @extinction_date == nil || @extinction_date <= 1900
+    if extinction_date == nil || extinction_date <= 1900
 		return false
 	else
 		true
